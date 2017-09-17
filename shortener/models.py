ef80bdc494641d246b2d4e2123e49807b95ec200
Ayukha <ayussh.khare@gmail.com>
2017-09-17 16:58:47 +0530

    Models updated

--- shortener/models.py
+++ shortener/models.py
@@ -1,11 +1,18 @@
 from django.db import models
-
+from .utils import code_generator , create_shortcode
 # Create your models here.
+
+
 class kirrURL (models.Model):
 	url = models.CharField(max_length=220,)
-	shortcode = models.CharField(max_length=15, unique=True)
+	shortcode = models.CharField(max_length=15, unique=True,blank=True)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now=True)
+
+	def save(self,*args,**kwargs):
+		if  shortcode is None or shortcode=="":
+			self.shortcode=create_shortcode(self)
+		super(kirrURL,self).save(*args,**kwargs)
 
 
 	def __str__(self):
